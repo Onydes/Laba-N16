f48f849de490acfade7c8071a0fb9165493294f6
Evaris <pud8573@gmail.com>
2025-04-15 17:48:45 +0300

    Create NotFoundPage.js

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+const NotFoundPage = () => {
+    return (
+      <main>
+        <h1>404 - Сторінку не знайдено</h1>
+        <p>Можливо, ви перейшли за невірним посиланням.</p>
+      </main>
+    );
+  };
+  
+  export default NotFoundPage;
+  
