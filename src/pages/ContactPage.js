6a1a1197f915917e2a75128ca768e79282419922
Evaris <pud8573@gmail.com>
2025-04-15 17:48:23 +0300

    Create ContactPage.js

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,17 @@
+const ContactPage = () => {
+  return (
+    <main>
+      <h1>Зв'яжіться з нами</h1>
+      <p>Ми завжди раді допомогти! Ось наші контактні дані:</p>
+      <ul>
+        <li>Email: support@ourcompany.com</li>
+        <li>Телефон: +38 (050) 765-43-21</li>
+        <li>Адреса: м. Київ, вул. Хрещатик, 22</li>
+      </ul>
+    </main>
+  );
+};
+
+export default ContactPage;
+
+  
